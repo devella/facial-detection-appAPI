856589a99ecc6c977ce7a90f4569adc8aaa4bda3
Samson <malonee20@gmail.com>
2020-02-29 15:09:45 +0200

    dtabase host change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./monitors/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1', 
+      host : 'postgresql-octagonal-54490', 
       user : 'postgres',
       password : 'home',
       database : 'facerecognition'
